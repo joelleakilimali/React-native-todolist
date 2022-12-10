ddf53d441290f3c9b4f26309c90b37f4e86201b9
joelleakilimali <akilimalijoelle@gmail.com>
2022-12-10 15:13:56 +0200

    finalization

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { Button, FlatList, Image, StyleSheet, View, Text } from "react-native";
 import { useState } from "react";
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   const [modalIsVisible, setmodalIsVisible] = useState(false);
@@ -24,46 +25,49 @@ export default function App() {
     setmodalIsVisible(false);
   };
   return (
-    <View style={styles.appContenaire}>
-      <Button
-        title="Add New Goal"
-        color="#5e0acc"
-        onPress={() => {
-          setmodalIsVisible(true);
-        }}
-      />
-      <View>
-        <GoalInput
-          addGoalHandler={onAddGoalHandler}
-          visible={modalIsVisible}
-          cancelGoal={endAddGoalHandler}
+    <>
+      <StatusBar style="light" />
+      <View style={styles.appContenaire}>
+        <Button
+          title="Add New Goal"
+          color="#a065ec"
+          onPress={() => {
+            setmodalIsVisible(true);
+          }}
         />
-      </View>
+        <View>
+          <GoalInput
+            addGoalHandler={onAddGoalHandler}
+            visible={modalIsVisible}
+            cancelGoal={endAddGoalHandler}
+          />
+        </View>
 
-      <View style={styles.goalContainer}>
-        <FlatList
-          data={listOfGoal}
-          renderItem={(itemData) => {
-            return (
-              <GoalItem
-                text={itemData.item.text}
-                onDeleteHandler={deleteGoalHandler}
-                id={itemData.item.id}
-              />
-            );
-          }}
-          alwaysBounceVertical={false}
-        ></FlatList>
+        <View style={styles.goalContainer}>
+          <FlatList
+            data={listOfGoal}
+            renderItem={(itemData) => {
+              return (
+                <GoalItem
+                  text={itemData.item.text}
+                  onDeleteHandler={deleteGoalHandler}
+                  id={itemData.item.id}
+                />
+              );
+            }}
+            alwaysBounceVertical={false}
+          ></FlatList>
+        </View>
+        <View>
+          <Image
+            style={styles.imageNote}
+            source={{
+              uri: "https://irp.cdn-website.com/fc4de732/dms3rep/multi/1+4.gif",
+            }}
+          />
+        </View>
       </View>
-      <View>
-        <Image
-          style={styles.imageNote}
-          source={{
-            uri: "https://irp.cdn-website.com/fc4de732/dms3rep/multi/1+4.gif",
-          }}
-        />
-      </View>
-    </View>
+    </>
   );
 }
 
@@ -72,7 +76,6 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 50,
     marginHorizontal: 16,
-    backgroundColor: "#1e085a",
   },
 
   goalContainer: {
